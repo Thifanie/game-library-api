2d06df909f6547e603f4ca937a27c0ad991425e0
Thifanie <thifanie640@gmail.com>
2026-02-23 11:34:47 +0100

    Implementation class GameRepository : add of storage, findAll() and findById()

--- src/repository/GameRepository.java
+++ src/repository/GameRepository.java
@@ -1,4 +1,21 @@
 package repository;
 
+import model.Game;
+
+import java.util.*;
+
 public class GameRepository {
+//    Création du stockage des jeux
+    private final Map<Long, Game> storage = new HashMap<>();
+    private long nextId = 1L;
+
+//    Méthode qui retourne un tableau contenant tous les jeux
+    public List<Game> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+//    Méthode qui retourne le jeu correspondant à l'ID et gère le cas "not found"
+    public Optional<Game> findById(long id) {
+        return Optional.ofNullable(storage.get(id));
+    }
 }
