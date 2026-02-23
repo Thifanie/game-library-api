2694b225f61b2f4c052c9c426d4de15885feea2c
Thifanie <thifanie640@gmail.com>
2026-02-23 12:49:49 +0100

    Implementation class GameRepository : add of save(), existsById() and deleteById()

--- src/repository/GameRepository.java
+++ src/repository/GameRepository.java
@@ -18,4 +18,26 @@ public class GameRepository {
     public Optional<Game> findById(long id) {
         return Optional.ofNullable(storage.get(id));
     }
+
+//    Méthode qui ajoute un nouveau jeu ou remplace un jeu existant
+    public Game save(Game game) {
+
+        if (game.getId() == null) {
+            // Création
+            game.setId(nextId++);
+        }
+
+        storage.put(game.getId(), game);
+        return game;
+    }
+
+//    Méthode qui vérifie si un ID correspond à un jeu existant
+    public boolean existsById(Long id) {
+        return storage.containsKey(id);
+    }
+
+//    Méthode qui supprime le jeu correspondant à l'ID et renvoie true si le jeu existe
+    public boolean deleteById(Long id) {
+        return storage.remove(id) != null;
+    }
 }
